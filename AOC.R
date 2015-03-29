88afd272d1f3c78b2a01f5b40df2e3d283d9b386
coulmont <coulmont@yahoo.com>
2015-03-29 19:54:11 +0200

    Code pour la cartographie

---
+++ AOC.R
@@ -0,0 +1,62 @@
+library(maptools)
+library(rgdal)
+library(sp)
+library(RColorBrewer)
+library(classInt)
+france<-readOGR("../../Documents/vieux-Documents/cartographie/geofla/COMMUNES/","COMMUNE")
+dataigp<-read.csv2("../../Downloads/2015-03-24-COMAGRI_-_Communes_Aires_IG_.csv",stringsAsFactors=F)
+dataaoc<-read.csv2("../../Downloads/2015-03-24-COMAGRI_-_Communes_Aires_AO.csv",stringsAsFactors=F)
+
+europe<-readOGR("NUTS_2010_03M_SH/Data","NUTS_RG_03M_2010") # données EUROSTAT
+frontieres<-subset(europe,europe$NUTS_ID=="FR") # on garde juste la France 
+frontieres<-spTransform (frontieres, CRS ("+init=epsg:2154") ) # change la projection en LAMBERT
+head(data)
+dataigp$CID<-sprintf("%05s",dataigp$CI)
+dataigp$NOMBRE<-1
+dataigp<-aggregate(NOMBRE~CID,data=dataigp,sum)
+dataaoc$CID<-sprintf("%05s",dataaoc$CI)
+dataaoc$NOMBRE<-1
+dataaoc<-aggregate(NOMBRE~CID,data=dataaoc,sum)
+data<-rbind(dataigp,dataaoc)
+data<-aggregate(NOMBRE~CID,data=data,sum)
+
+#df$NUM_DEP<-sprintf("%02d",df$V3)
+summary(france)
+m <- match(france$INSEE_COM, data$CID)
+france$VAR <- data$NOMBRE[m]
+nclr <- 11
+plotclr <- brewer.pal(nclr,"RdYlGn")[nclr:1] 
+class <- classIntervals(france$VAR, nclr, style="quantile",dataPrecision=0)
+colcode <- findColours(class, plotclr)
+colcode[is.na(colcode)]<-"#999999"
+pdf("../../Desktop/france-test2.pdf",width=8,height=8)
+par(mar=c(1,2,2,1))
+plot(france, col=colcode, border=colcode, lwd=.1) #border="#ffffff00"
+#plot(subset(france,france$INSEE_COM=="10387"),add=T,col="black")
+plot(frontieres,add=T,col="#ffffff00",xlim=c(99226,1242375),ylim=c(6049647,7110524))
+title(main="Nombre d'AOC, AOP et IGP par commune",line=-1)
+title(sub="Source : INAO sur data.gouv.fr | Réalisation Baptiste Coulmont \nhttp://coulmont.com/blog/2015/03/29/aoc-igp",line=-5 ,cex.sub=.7)
+legend(090224,6711753,legend=c(names(attr(colcode,"table"))[nclr:1],"N.A."), fill=c(attr(colcode, "palette")[nclr:1],"#999999"), cex=1, bty="n")
+dev.off()
+
+head(data)
+
+summary(france)
+m<-match(france$INSEE_COM,data$CID)
+france$VAR<-data$NOMBRE[m]
+france$VAR<-100*france$VAR/france$SUPERFICIE
+nclr <- 7
+plotclr <- brewer.pal(nclr,"RdYlGn")[nclr:1] 
+class <- classIntervals(france$VAR, nclr, style="quantile",dataPrecision=1)
+colcode <- findColours(class, plotclr)
+colcode[is.na(colcode)]<-"#999999"
+pdf("../../Desktop/france-test-superficie.pdf",width=8,height=8)
+par(mar=c(1,2,2,1))
+plot(france,col=colcode,border="#ffffff00")
+plot(frontieres,add=T,col="#ffffff00",xlim=c(99226,1242375),ylim=c(6049647,7110524))
+title(main="Nombre d' AOC/AOP/IGP par km2 par commune",line=-1)
+title(sub="Source : INAO sur data.gouv.fr | Réalisation Baptiste Coulmont \nhttp://coulmont.com/blog/2015/03/29/aoc-igp",line=-5 ,cex.sub=.7)
+legend(090224,6711753,legend=c(names(attr(colcode,"table"))[nclr:1],"N.A."), fill=c(attr(colcode, "palette")[nclr:1],"#999999"), cex=1, bty="n")
+dev.off()
+
+
